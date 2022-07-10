bbb1c0c2a97796f6e1de1a937373188b8090aa55
Paul <pavel.m.pashax12@gmail.com>
2022-07-10 15:47:17 +0300

    add Main method file

---
+++ src/main/java/by/paul/idftesttask/IdfTestTaskApplication.java
@@ -0,0 +1,13 @@
+package by.paul.idftesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfTestTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IdfTestTaskApplication.class, args);
+  }
+
+}
