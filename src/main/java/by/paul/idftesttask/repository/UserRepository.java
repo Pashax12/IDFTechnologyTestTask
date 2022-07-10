15a24d29f01be2f5537b32a630044b259e9ba972
Paul <pavel.m.pashax12@gmail.com>
2022-07-10 19:00:03 +0300

    added method of parsing

--- src/main/java/by/paul/idftesttask/repository/UserRepository.java
+++ src/main/java/by/paul/idftesttask/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package by.paul.idftesttask.repository;
 
 import by.paul.idftesttask.entity.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends  CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
+  @Query(value = "SELECT * FROM Users u "
+      + "WHERE u.currency_code = ?1 and "
+      + "((?2 - u.price_usd)/u.price_usd >0.01 OR (?2 - u.price_usd)/u.price_usd < -0.01)",
+      nativeQuery = true)
+  List<User> findAllByCurrencyCodeAndPriceUsd(Long currencyCode, Double actualPrice);
 
 }
